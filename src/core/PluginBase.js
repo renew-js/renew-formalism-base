cb16da25d5d1fa4341e72ccdb1ae3946c402f43c
Marc Richter <marc@richter.bz>
2019-03-02 18:04:04 +0100

    Initial commit

---
+++ src/core/PluginBase.js
@@ -0,0 +1,37 @@
+import Formalism from 'renew-formalism';
+
+import metamodel from '../ontology/MetaModel.json';
+import stylesheet from '../ontology/Stylesheet.json';
+import toolConfiguration from '../ontology/ToolConfiguration.json';
+
+
+/**
+ *
+ */
+export default class PluginPT extends Formalism.Plugin {
+    constructor (props) {
+        super(props);
+        this.type = metamodel.type;
+    }
+
+    /**
+     * @return {MetaModel}
+     */
+    getMetaModel () {
+        return Formalism.Ontology.MetaModel.fromJson(metamodel);
+    }
+
+    /**
+     * @return {Stylesheet}
+     */
+    getStylesheet () {
+        return Formalism.Ontology.Stylesheet.fromJson(stylesheet);
+    }
+
+    /**
+     * @return {ToolConfiguration}
+     */
+    getToolConfiguration () {
+        return Formalism.Ontology.ToolConfiguration.fromJson(toolConfiguration);
+    }
+}
